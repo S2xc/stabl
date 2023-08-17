adc35beec712105d738939ed976e0236e39cdb52
Ilya Gerasimov <py1lya@yandex.ru>
2023-08-17 20:52:22 +0300

    git

--- round.py
+++ round.py
@@ -1,6 +1,8 @@
-print(round(123.42, -1))
-print(round(123.42, -2))
-print(round(123.42, -3))
+print(round(123.42, -1)) # -> 120.0
+print(round(123.42, -2)) # -> 100.0
+print(round(123.42, -3)) # -> 0.0
+
+print('Hi git')
 
 
 
